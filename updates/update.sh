b099bc1852e16c6cc0c23e3eed96d2f7c3a1bb06
rpi-ha <seelive@pobox.com>
2019-09-02 13:20:17 -0400

    Update update.sh

--- updates/update.sh
+++ updates/update.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
 # add lines to run updates
-LOG="update.log"
-#cd /usr/local/sarah/updates
-cd ${0%/*}
-
-echo `date`
-echo "    ${0%/*}"
-
-echo `date` >> $LOG
-echo "    ${0%/*}" >> $LOG
